94de38e9c62d6dc8b3e2b87907389bab831ecd9b
ramaswamy11703 <ramaswamy@gmail.com>
2017-02-17 18:13:01 -0800

    small typo

--- src/main/java/com/ramaswamy/neil/HelloAppEngine.java
+++ src/main/java/com/ramaswamy/neil/HelloAppEngine.java
@@ -390,7 +390,7 @@ public class HelloAppEngine extends HttpServlet {
 
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) 
-			throws IOException {
+			throws IOException, ServletException {
 		doGet(request, response);
 	}
 
